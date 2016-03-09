ab5cd31041054cb4fb4530085e8ff03e8f31779c
dikmedvescekmurovec <dm3825@student.uni-lj.si>
2016-03-09 14:59:05 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,9 +1,20 @@
+
+
 window.addEventListener('load', function() {
 	//stran nalozena
-		
+
+	var izvediPrijavo = function(event){
+		var uporabnikIme = document.querySelector("#uporabnisko_ime").value;
+		var pokrivalo = document.querySelector(".pokrivalo");
+		pokrivalo.style.display = "none";
+		var ime = document.querySelector("#uporabnik");
+		ime.textContent = uporabnikIme;
+	}
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
+		var i = 0;
 		
 		for (i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
@@ -15,6 +26,7 @@ window.addEventListener('load', function() {
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
 		}
 	}
-	setInterval(posodobiOpomnike, 1000);
 	
+	setInterval(posodobiOpomnike, 1000);
 });
+
